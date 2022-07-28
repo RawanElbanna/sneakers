aca4e38e6efd822986bdb81275f6062056d18f14
RawanElbanna <99205603+RawanElbanna@users.noreply.github.com>
2022-07-28 22:45:20 +0200

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -1,1 +1,38 @@
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "block";
+  }
+  let subs = document.querySelector(".discount");
+let plus = document.querySelector(".count");
+let number = document.querySelector(".number");
+let start = 1;
 
+
+subs.addEventListener("click", function() {
+  number.classList.toggle("add");
+  start -= 1;
+  number.innerHTML = start;
+});
+
+plus.addEventListener("click", function() {
+  number.classList.toggle("add");
+  start += 1;
+  number.innerHTML = start;
+});
+
+function myFunctionn() {
+    var x = document.getElementById("options");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+//   $(document).ready(function(){
+//     $('.thumbnails a').click(function(e){
+//       e.preventDefault();
+// $('.image-gallery img').attr('src',$(this).attr('href'));
+//     })
+//   });
